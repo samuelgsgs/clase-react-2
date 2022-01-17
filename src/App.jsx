67371b68c6e3d8192e4e675b214ef2cb168da48a
samuelgsgs <tabletsamugs@gmail.com>
2022-01-17 14:03:18 +0100

    17/01, final clase

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import { Home } from './components/Home';
 import { PaginaListas } from './components/PaginaListas';
 import { Perfil } from './components/Perfil';
 import Header from './components/Header';
+import BootstrapHeader from './components/BootstrapHeader';
 import { MenuItems } from './data/MenuItems';
 
 export function App() {
   return (
     <Router>
       <Header />
+      <BootstrapHeader />
       {MenuItems.map((item) => {
         return (
           <Route
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Table } from 'react-bootstrap';
 
 export function Home() {
   return (
     <div className="main-site">
       <h1>Bienvenido!</h1>
+
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Nombre</th>
+            <th>Primer Apellido</th>
+            <th>Nombre Usuario</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Samuel</td>
+            <td>Gómez</td>
+            <td>@Samuelgsgs</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Manuel</td>
+            <td>David</td>
+            <td>@Supermanu545</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Alfredo López</td>
+            <td>@Alfred2001</td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   );
 }
