d35c1e91bf3731175c42a74ff35adf94b9967e45
samuelgsgs <tabletsamugs@gmail.com>
2022-01-18 12:29:56 +0100

    Clase 18/01, con todos los coches y fotos añadidas

---
+++ src/data/DatosCoches.js
@@ -0,0 +1,47 @@
+export const TitulosTablaCoches = {
+  id: 'matricula',
+  field1: 'Marca',
+  field2: 'Modelo',
+  field3: 'Color',
+  field4: 'Imagen',
+  field5: 'Descripción',
+};
+
+export const DatosTablaCoches = [
+  {
+    matricula: 'M YX 5012',
+    marca: 'BMW',
+    modelo: 'M8',
+    color: 'Azul Mate',
+    imagen:
+      'https://d1eip2zddc2vdv.cloudfront.net/dphotos/2659349-1559749029.jpeg',
+    descripción: 'Fascinante',
+  },
+  {
+    matricula: 'KE U1',
+    marca: 'AUDI',
+    modelo: 'S8',
+    color: 'Negro',
+    imagen:
+      'https://www.tuningblog.eu/wp-content/uploads/2020/06/2020-ABT-Sportsline-Audi-S8-D5-Tuning-Header.jpg',
+    descripción: 'Elegante y servicial',
+  },
+  {
+    matricula: 'S GT 1014',
+    marca: 'MERCEDES',
+    modelo: 'AMG GTS',
+    color: 'Azul Oscuro',
+    imagen:
+      'https://cdn.topgear.es/sites/navi.axelspringer.es/public/media/image/2020/07/mercedes-amg-gt-2021-2014909.jpg',
+    descripción: 'Un coche genial',
+  },
+  {
+    matricula: '0022 CUV',
+    marca: 'SEAT',
+    modelo: 'Cupra Formentor',
+    color: 'Gris Mate',
+    imagen:
+      'https://www.cupraofficial.es/content/dam/public/cupra-website/cars/cupra-range/formentor/overlay-design-exterior/gallery-component-features-sophisticated-sporting-prime/x-large/01-new-cupra-formentor-exclusive-petrol-blue-matte-exterior-finish.jpg',
+    descripción: 'Barato y Comprometedor',
+  },
+];
